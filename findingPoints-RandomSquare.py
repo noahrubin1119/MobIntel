e0574371506d6d57416c89fe061f77bfa15a5992
noahrubin1119 <35118067+noahrubin1119@users.noreply.github.com>
2020-07-23 14:12:06 -0400

    made the output neater

--- findingPoints-RandomSquare.py
+++ findingPoints-RandomSquare.py
@@ -3,19 +3,19 @@ import random
 
 square = {
     "southwest": {
-        "lng": 100,
-        "lat": 200
+        "lng": 0,
+        "lat": 0
         },
     "northeast": {
         "lng": 200,
-        "lat": 300
+        "lat": 200
         }
     }
 
 print(square)
 
 
-def gen_points(square, n = 4):
+def gen_points(square, n = 5):
     y1 = square.get("southwest").get("lng")
     x1 = square.get("southwest").get("lat")
     y2 = square.get("northeast").get("lng")
@@ -34,6 +34,10 @@ def gen_points(square, n = 4):
         
     }
 
+    points = {
+
+        }
+
     numRows = math.ceil(math.sqrt(n))
     print("numRows: " + str(numRows))
     numSubSquares = (numRows ** 2)
@@ -48,7 +52,7 @@ def gen_points(square, n = 4):
             "row": row,
             "col": col
                          }
-    
+    c = 0
     for subSquare in subSquares.items():
         row = subSquare[1]['row']
         col = subSquare[1]['col']
@@ -61,8 +65,13 @@ def gen_points(square, n = 4):
         subSquares[subSquare[0]]["y1"] = y1 + rowOffset
         subSquares[subSquare[0]]["x"] = x1 + colOffset + (sideLen/numRows)/2
         subSquares[subSquare[0]]["y"] = y1 + rowOffset + (sideLen/numRows)/2
-
-    print(subSquares)
+        
+        points[c] = {"x": x1 + colOffset + (sideLen/numRows)/2,
+                     "y": y1 + rowOffset + (sideLen/numRows)/2
+                     }
+        c = c+1
+        
+    print(points)
 
 
 gen_points(square)
